0cbfd77180ae68729cfa44d5f3b99b78562dee81
Lmao1235 <148939689+Lmao1235@users.noreply.github.com>
2025-01-25 02:11:34 +0700

    LevelSelection

---
+++ Assets/Script/SwitchLevels.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevels : MonoBehaviour
+{
+    public int Level1;
+    public int Level2;
+    public int Level3;
+
+    public void Lvl1()
+    {
+        SceneManager.LoadScene(Level1, LoadSceneMode.Single);
+    }
+    public void Lvl2()
+    {
+        SceneManager.LoadScene(Level2, LoadSceneMode.Single);
+    }
+    public void Lvl3()
+    {
+        SceneManager.LoadScene(Level3, LoadSceneMode.Single);
+    }
+
+   
+}
